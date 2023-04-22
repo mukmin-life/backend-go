b388d4fb35e0ab39033d34c4a1956a8a1f3fed45
Mohamad Fadhil <piukul@yahoo.com.my>
2023-04-22 16:51:09 +0800

    Revert "add otel tracing"
    This reverts commit bb609e21fc16ab0606d384b612ea4d86d868d1e3.

--- main.go
+++ main.go
@@ -5,20 +5,11 @@ import (
 	_ "github.com/mukmin-life/docs"
 	"github.com/mukmin-life/db"
 	prayer_api "github.com/mukmin-life/modules/prayer_time"
-	"context"
 
 	"log"
-	"os"
 
 	"github.com/gofiber/fiber/v2"
 	swagger "github.com/swaggo/fiber-swagger"
-
-	texporter "github.com/GoogleCloudPlatform/opentelemetry-operations-go/exporter/trace"
-	"go.opentelemetry.io/contrib/detectors/gcp"
-	"go.opentelemetry.io/otel"
-	"go.opentelemetry.io/otel/sdk/resource"
-	sdktrace "go.opentelemetry.io/otel/sdk/trace"
-	semconv "go.opentelemetry.io/otel/semconv/v1.7.0"
 )
 
 // @title Mukmin Life API
@@ -32,34 +23,7 @@ import (
 // @host localhost:3000
 // @BasePath /v2
 func main() {
-
-	// Initiate OTEL tracing
-	// Ref: https://cloud.google.com/trace/docs/setup/go-ot
-	ctx := context.Background()
-	projectID := os.Getenv("GOOGLE_CLOUD_PROJECT")
-	exporter, err := texporter.New(texporter.WithProjectID(projectID))
-	if err == nil {
-		// Identify your application using resource detection
-		res, err := resource.New(ctx,
-				resource.WithDetectors(gcp.NewDetector()),
-				resource.WithTelemetrySDK(),
-				resource.WithAttributes(
-						semconv.ServiceNameKey.String("mukmin-api"),
-				),
-		)
-		if err != nil {
-				log.Fatalf("resource.New: %v", err)
-		}
-
-		tp := sdktrace.NewTracerProvider(
-				sdktrace.WithBatcher(exporter),
-				sdktrace.WithResource(res),
-		)
-		defer tp.ForceFlush(ctx)
-		otel.SetTracerProvider(tp)
-	}
-
-    err = db.Connect()
+    err := db.Connect()
     if err != nil {
         log.Fatal("failed to connect to DB")
     }
